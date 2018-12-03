a76aa1ec8c41ddeca202174ffc54120eb3b21b42
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2018-12-03 14:58:55 +0200

    first commit

---
+++ szerver/cmake-build-debug/CMakeFiles/szerver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/szerver.dir/main.cpp.obj"
+  "szerver.pdb"
+  "szerver.exe"
+  "szerver.exe.manifest"
+  "libszerver.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/szerver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
