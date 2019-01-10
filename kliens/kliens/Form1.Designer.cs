d87d16caf2d12501f69006ff7bf1871d4d2eb5a1
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2019-01-11 00:02:21 +0200

    Megy

--- kliens/kliens/Form1.Designer.cs
+++ kliens/kliens/Form1.Designer.cs
@@ -32,15 +32,12 @@
             this.messages = new System.Windows.Forms.RichTextBox();
             this.message = new System.Windows.Forms.TextBox();
             this.name = new System.Windows.Forms.TextBox();
-            this.ip = new System.Windows.Forms.TextBox();
-            this.port = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.connect = new System.Windows.Forms.Button();
             this.userslist = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
             this.loadusers = new System.Windows.Forms.Button();
+            this.exitPm = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Send
@@ -71,24 +68,10 @@
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(305, 18);
+            this.name.Location = new System.Drawing.Point(305, 20);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(100, 20);
             this.name.TabIndex = 4;
-            // 
-            // ip
-            // 
-            this.ip.Location = new System.Drawing.Point(305, 57);
-            this.ip.Name = "ip";
-            this.ip.Size = new System.Drawing.Size(100, 20);
-            this.ip.TabIndex = 5;
-            // 
-            // port
-            // 
-            this.port.Location = new System.Drawing.Point(305, 96);
-            this.port.Name = "port";
-            this.port.Size = new System.Drawing.Size(100, 20);
-            this.port.TabIndex = 6;
             // 
             // label1
             // 
@@ -99,27 +82,9 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Name";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(336, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "IP";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(336, 80);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Port";
-            // 
             // connect
             // 
-            this.connect.Location = new System.Drawing.Point(322, 122);
+            this.connect.Location = new System.Drawing.Point(322, 81);
             this.connect.Name = "connect";
             this.connect.Size = new System.Drawing.Size(75, 23);
             this.connect.TabIndex = 10;
@@ -132,8 +97,9 @@
             this.userslist.FormattingEnabled = true;
             this.userslist.Location = new System.Drawing.Point(411, 20);
             this.userslist.Name = "userslist";
-            this.userslist.Size = new System.Drawing.Size(120, 134);
+            this.userslist.Size = new System.Drawing.Size(155, 134);
             this.userslist.TabIndex = 11;
+            this.userslist.SelectedIndexChanged += new System.EventHandler(this.userslist_SelectedIndexChanged);
             // 
             // label4
             // 
@@ -146,7 +112,7 @@
             // 
             // loadusers
             // 
-            this.loadusers.Location = new System.Drawing.Point(430, 165);
+            this.loadusers.Location = new System.Drawing.Point(411, 165);
             this.loadusers.Name = "loadusers";
             this.loadusers.Size = new System.Drawing.Size(75, 23);
             this.loadusers.TabIndex = 13;
@@ -154,26 +120,33 @@
             this.loadusers.UseVisualStyleBackColor = true;
             this.loadusers.Click += new System.EventHandler(this.loadusers_Click);
             // 
+            // exitPm
+            // 
+            this.exitPm.Location = new System.Drawing.Point(492, 165);
+            this.exitPm.Name = "exitPm";
+            this.exitPm.Size = new System.Drawing.Size(75, 23);
+            this.exitPm.TabIndex = 14;
+            this.exitPm.Text = "Exit Pm";
+            this.exitPm.UseVisualStyleBackColor = true;
+            this.exitPm.Click += new System.EventHandler(this.exitPm_Click);
+            // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 205);
+            this.ClientSize = new System.Drawing.Size(578, 208);
+            this.Controls.Add(this.exitPm);
             this.Controls.Add(this.loadusers);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.userslist);
             this.Controls.Add(this.connect);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.port);
-            this.Controls.Add(this.ip);
             this.Controls.Add(this.name);
             this.Controls.Add(this.message);
             this.Controls.Add(this.Send);
             this.Controls.Add(this.messages);
             this.Name = "Chat";
-            this.Text = "Form1";
+            this.Text = "Chat";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,15 +158,12 @@
         private System.Windows.Forms.RichTextBox messages;
         private System.Windows.Forms.TextBox message;
         private System.Windows.Forms.TextBox name;
-        private System.Windows.Forms.TextBox ip;
-        private System.Windows.Forms.TextBox port;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button connect;
         private System.Windows.Forms.ListBox userslist;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button loadusers;
+        private System.Windows.Forms.Button exitPm;
     }
 }
 
--- kliens/kliens/Form1.cs
+++ kliens/kliens/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
 
 namespace kliens
 {
@@ -16,27 +18,162 @@ namespace kliens
         private int Port;
         private String Ip;
         private String uName;
+        private TcpClient socket=null;
+        private int pm = 0;
         public Chat()
         {
             InitializeComponent();
         }
+        private byte[] ReadBytes(int count)
+        {
+            NetworkStream networkStream = socket.GetStream();
 
+            byte[] bytes = new byte[count]; // buffer to fill (and later return)
+            int readCount = 0; // bytes is empty at the start
+
+            // while the buffer is not full
+            while (readCount < count)
+            {
+                // ask for no-more than the number of bytes left to fill our byte[]
+                int left = count - readCount; // we will ask for `left` bytes
+                int r = networkStream.Read(bytes, readCount, left); // but we are given `r` bytes (`r` <= `left`)
+
+                if (r == 0)
+                {
+                    throw new Exception("Lost Connection during read");
+                }
+
+                readCount += r; // advance by however many bytes we read
+            }
+
+            return bytes;
+        }
+        private string ReadMessage()
+        {
+            // read length bytes, and flip if necessary
+            byte[] lengthBytes = ReadBytes(sizeof(int)); // int is 4 bytes
+            if (System.BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthBytes);
+            }
+
+            // decode length
+            int length = System.BitConverter.ToInt32(lengthBytes, 0);
+
+            // read message bytes
+            byte[] messageBytes = ReadBytes(length);
+
+            // decode the message
+            string message = System.Text.Encoding.ASCII.GetString(messageBytes);
+
+            return message;
+        }
+        private void read()
+        {
+
+            while (true)
+            {
+                string message = ReadMessage();
+                if (message != "")
+                {
+                    if (message.Contains("\\listusers:"))
+                    {
+                        //usereket kapja meg
+                        string[] users = message.Split(' ');
+                        foreach (var user in users)
+                        {
+                            if (user != "\\listusers:")
+                            {
+                                Invoke(new Action(() =>
+                                {
+                                    userslist.Items.Add(user);
+                                }));
+                            }
+                        }
+                    }
+                    else if(message.Contains("\\PMexit") == false && message.Contains("\\pm with")==false && message.Contains("\\listusers") == false)
+                    {
+                        //minki latja ezt az uzenetet
+                        Invoke(new Action(() =>
+                        {
+                            messages.AppendText(message + "\r\n");
+                        }));
+                    }
+                }
+            }
+        }
+        private void send(String m)
+        {
+            if (socket == null)
+            {
+                MessageBox.Show("Not connected", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            NetworkStream networkStream = socket.GetStream();
+            byte[] messageBytes = Encoding.ASCII.GetBytes(m);
+            int length = messageBytes.Length;
+            byte[] lengthBytes = System.BitConverter.GetBytes(length);
+            if (System.BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthBytes);
+            }
+            networkStream.Write(lengthBytes, 0, lengthBytes.Length);
+            networkStream.Write(messageBytes, 0, length);
+        }
         private void connect_Click(object sender, EventArgs e)
         {
             uName = name.Text;
-            Ip = ip.Text;
-            Port = Convert.ToInt32(port.Text);
+            if (uName == "")
+            {
+                MessageBox.Show("No username", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Ip = "127.0.0.1";
+            Port = 10013;
+            try
+            {
+                socket = new TcpClient(Ip, Port);
+            }
+            catch(SocketException socketException)
+            {
+                Debug.WriteLine("Socket exception: " + socketException);
+                socket = null;
+                MessageBox.Show("Server error", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("Sucesfuly connected", "Connected",
+                MessageBoxButtons.OK, MessageBoxIcon.None);
+            var t = new Thread(read);
+            t.Start();
+            send(uName);
         }
 
         private void Send_Click(object sender, EventArgs e)
         {
             String text = message.Text;
-
+            send(text);
         }
 
         private void loadusers_Click(object sender, EventArgs e)
         {
+            send("\\listusers");
+        }
 
+        private void exitPm_Click(object sender, EventArgs e)
+        {
+            send("\\PMexit");
+            pm = 0;
+        }
+
+        private void userslist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string u = userslist.SelectedItem.ToString();
+            pm = 1;
+            send("\\pm with "+u);
+            messages.AppendText("You are talking with "+u+"\n");
         }
     }
 }
