c09ef40cec39d4fcd98dbdf78ba730fbb0efb9f2
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2019-01-11 00:00:23 +0200

    Megy

---
+++ szerver/ChatThread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by farka on 1/3/2019.
+//
+
+#include "ChatThread.h"
---
+++ szerver/ChatThread.h
@@ -0,0 +1,14 @@
+//
+// Created by farka on 1/3/2019.
+//
+
+#ifndef SZERVER_CHATTHREAD_H
+#define SZERVER_CHATTHREAD_H
+
+
+class ChatThread {
+
+};
+
+
+#endif //SZERVER_CHATTHREAD_H
